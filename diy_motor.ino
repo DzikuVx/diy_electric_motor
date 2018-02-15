0aaf952278b6ceffce139221e8c1c2e70a51116d
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2018-02-15 08:07:02 +0100

    OLED disaplay and voltage monitoring

--- diy_motor.ino
+++ diy_motor.ino
@@ -1,4 +1,8 @@
 #include "Arduino.h"
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 #define IR_LED_PIN 9
 #define IR_FREQUENCY 5000
@@ -17,7 +21,8 @@
 void setup()
 {
 	pinMode(IR_LED_PIN, OUTPUT);
-    tone(IR_LED_PIN, IR_FREQUENCY);
+    // tone(IR_LED_PIN, IR_FREQUENCY);
+    analogWrite(IR_LED_PIN, 64);
 
     pinMode(IR_RECEVER_PIN, INPUT);
     pinMode(POWER_OUTPUT_PIN, OUTPUT);
@@ -27,18 +32,24 @@ void setup()
     pinMode(VOLTAGE_MONITORING_PIN, INPUT);
 
     Serial.begin(115200);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 128x32)
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.clearDisplay();
+    display.display();
 }
 
 void loop()
 {
     
-    int vBatAdc = analogRead(VOLTAGE_MONITORING_PIN);
+    static uint32_t lastOledUpdate = 0;
+    static uint8_t prevStatus;
 
+    int vBatAdc = analogRead(VOLTAGE_MONITORING_PIN);
     int vBat = map(vBatAdc, 0, 430, 0, 121);
 
-    Serial.print(vBatAdc);
-    Serial.print(" ");
-    Serial.println(vBat);
+    bool oledOpen = false;
 
     static uint8_t status = LOW;
     static uint32_t lastLowInput = millis();
@@ -53,11 +64,34 @@ void loop()
         status = HIGH;
     }
 
-    digitalWrite(SIGNAL_PIN, status);
+    // digitalWrite(SIGNAL_PIN, status);
 
     if (status == LOW) {
         analogWrite(POWER_OUTPUT_PIN, 100);
     } else {
         analogWrite(POWER_OUTPUT_PIN, 0);
     }
+
+    if (prevStatus == LOW && status == HIGH) {
+        oledOpen = true;
+    } else {
+        oledOpen = false;
+    }
+
+    prevStatus = status;
+
+    if (millis() - lastOledUpdate > 300 && oledOpen) {
+        display.clearDisplay();
+
+        display.setTextSize(1);
+        display.setCursor(0, 0);
+        display.print("Vbat: ");
+        display.println(vBat / 10.f);
+        display.println("");
+
+        display.display();
+
+        lastOledUpdate = millis();
+    }
+
 }
