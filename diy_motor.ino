feae400e93c045fb047886d401f0709a4faef218
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2018-02-15 18:22:56 +0100

    Events

--- diy_motor.ino
+++ diy_motor.ino
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include <Adafruit_SSD1306.h>
+#include <Wire.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
@@ -21,7 +22,6 @@ Adafruit_SSD1306 display(OLED_RESET);
 void setup()
 {
 	pinMode(IR_LED_PIN, OUTPUT);
-    // tone(IR_LED_PIN, IR_FREQUENCY);
     analogWrite(IR_LED_PIN, 64);
 
     pinMode(IR_RECEVER_PIN, INPUT);
@@ -32,6 +32,8 @@ void setup()
     pinMode(VOLTAGE_MONITORING_PIN, INPUT);
 
     Serial.begin(115200);
+
+    Wire.setClock(400000);
 
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // initialize with the I2C addr 0x3C (for the 128x32)
     display.setTextSize(1);
@@ -45,13 +47,14 @@ void loop()
     
     static uint32_t lastOledUpdate = 0;
     static uint8_t prevStatus;
+    static bool electromagnetEnabled = false;
 
     int vBatAdc = analogRead(VOLTAGE_MONITORING_PIN);
     int vBat = map(vBatAdc, 0, 430, 0, 121);
 
     bool oledOpen = false;
 
-    static uint8_t status = LOW;
+    static uint8_t shaftSensorStatus = LOW;
     static uint32_t lastLowInput = millis();
     
     if (digitalRead(IR_RECEVER_PIN) == LOW) {
@@ -59,26 +62,34 @@ void loop()
     }
 
     if (millis() - lastLowInput > LOW_LOCK_TIME) {
-        status = LOW;
+        shaftSensorStatus = LOW;
     } else {
-        status = HIGH;
+        shaftSensorStatus = HIGH;
     }
 
-    // digitalWrite(SIGNAL_PIN, status);
+    oledOpen = false;
 
-    if (status == LOW) {
-        analogWrite(POWER_OUTPUT_PIN, 100);
+    // Magnet is in closest position to electromagnet 
+    if (prevStatus == LOW && shaftSensorStatus == HIGH) {
+       oledOpen = true; // Allow OLED to update
+
+       //Disable electromagnet
+       electromagnetEnabled = false;
+    }
+
+    // Shaft is in furthest dedectable distance from the electromagnet
+    if (prevStatus == HIGH && shaftSensorStatus == LOW) {
+        electromagnetEnabled = true;
+    }
+
+    prevStatus = shaftSensorStatus;
+
+    // Cycle electromagnet is needed
+    if (electromagnetEnabled) {
+        analogWrite(POWER_OUTPUT_PIN, 80);
     } else {
         analogWrite(POWER_OUTPUT_PIN, 0);
     }
-
-    if (prevStatus == LOW && status == HIGH) {
-        oledOpen = true;
-    } else {
-        oledOpen = false;
-    }
-
-    prevStatus = status;
 
     if (millis() - lastOledUpdate > 300 && oledOpen) {
         display.clearDisplay();
